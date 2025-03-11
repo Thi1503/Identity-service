3581bcf0032c836c99c594694a6f1bf876095d8b
Thi1503 <thile15032003@gmail.com>
2025-03-11 18:37:59 +0700

    First commit

---
+++ identity-service/src/main/java/com/example/identity_service/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.identity_service.service;
+
+import com.example.identity_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+
+}
